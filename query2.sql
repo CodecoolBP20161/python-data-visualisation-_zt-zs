96e27aaeb1d78ec446e1b2d342a8719c1505b818
arvisura87 <jakab.tamas@me.com>
2016-07-13 11:38:22 +0200

    query 2 bug fixed

--- query2.sql
+++ query2.sql
@@ -3,8 +3,8 @@
 -- -  the color of the text should come from the database (project.main_color)
 
 SELECT name AS project_name,
-CASE WHEN budget_currency='USD' THEN round(budget_value::real / 1.11)
-     WHEN budget_currency='GBP' THEN round(budget_value::real * 1.18)
+CASE WHEN budget_currency='USD' THEN round(budget_value::real * 1.11)
+     WHEN budget_currency='GBP' THEN round(budget_value::real / 1.18)
      END AS budget_EUR,
                main_color
  FROM project

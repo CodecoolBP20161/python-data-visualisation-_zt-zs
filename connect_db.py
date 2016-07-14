08e97c6f9cf756b352d2fd3a78b51f99eeafd848
cickib <bellakocsis06@gmail.com>
2016-07-14 15:30:01 +0200

    colors are back

--- connect_db.py
+++ connect_db.py
@@ -25,21 +25,28 @@ def connect_params():
 # converting hex values to rgb triplets
 def hex_to_rgb(color_comps):
     if color_comps:
-        color_comps = color_comps.split()
-        rgb_colors = []
-        for hex_value in color_comps:
-            hex_value = hex_value.lstrip('#')
-            lv = len(hex_value)
-            rgb_colors.append(tuple(int(hex_value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3)))
-        color_counter = 0
-        avg_color = [0, 0, 0]
-        for triplet in rgb_colors:
-            color_counter += 1
-            for i in range(0, 3):
-                avg_color[i] += triplet[i]
-        for i in avg_color:
-            i /= color_counter
-        return tuple(avg_color)
+        if len(color_comps) > 4:
+            color_comps = color_comps.split()
+            rgb_colors = []
+            for hex_value in color_comps:
+                hex_value = hex_value.lstrip('#')
+                lv = len(hex_value)
+                rgb_colors.append(tuple(int(hex_value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3)))
+            if len(rgb_colors) > 1:
+                color_counter = 0
+                avg_color = [0, 0, 0]
+                for triplet in rgb_colors:
+                    color_counter += 1
+                    for i in range(0, 3):
+                        avg_color[i] += triplet[i]
+                # for i in avg_color:
+                #     # i /= color_counter
+                #     i += 100
+                return tuple(avg_color)
+            else:
+                return rgb_colors[0]
+        else:
+            return color_comps
     else:
         return(0, 0, 0)
 
--- image-generator1.py
+++ image-generator1.py
@@ -9,6 +9,7 @@ from hex_to_rgb import hex_to_rgb
 # very 'in progress'
 class Text():
     QUERIES = [sql_queries()[0], sql_queries()[1]]
+
     def __init__(self, queries):
         self.fill = queries[2]
         self.size = queries[1]
@@ -18,38 +19,62 @@ class Text():
     @classmethod
     def output_first(cls, queries):
         return [Text(queries) for queries in cls.QUERIES[0]]
+
+    @classmethod
     def output_second(cls, queries):
         return [Text(queries) for queries in cls.QUERIES[1]]
 
+    @staticmethod
+    def sizing(resize_list):
+        for d in resize_list:
+            if d.size is not None:
+                if d.size <= 10:
+                    d.size = int(d.size*10)
+                elif d.size > 10 and d.size <= 100:
+                    d.size = int(d.size//120)
+                elif d.size > 100 and d.size <= 1000:
+                    d.size = int(d.size // 150)
+                elif d.size > 1000:
+                    d.size = int(d.size//180)
+            else:
+                d.size = 11
+        return resize_list
 
 
-companiess = Text.output_first(sql_queries()[0])
-projects = Text.output_first(sql_queries()[1])
-
-img = Image.new("RGBA", (512, 512), "black")
+img = Image.new("RGB", (512, 512), "white")
 draw = ImageDraw.Draw(img)
+
 
 default_size = 10
 try:
     font_path = "/usr/share/fonts/FreeSans.ttf"
     # font = ImageFont.truetype(font_path, default_size)
     font = ImageFont.truetype(font_path)
-
 except:
     font = ImageFont.load_default()
 
 
+companies = Text.output_first(sql_queries()[0])
+projects = Text.output_second(sql_queries()[1])
+Text.sizing(companies)
+Text.sizing(projects)
+
+
 # generating an x coordinate
-def get_x(i):
-    text_size = draw.textsize(i, font=font)  # text size according to the font size
-    x = random.randint(0, (img.size[0]-text_size[0]))  # full width - horizontal size of text
+def get_x(text_size):
+    if text_size[0] >= img.size[0]:
+        x = 0
+    else:
+        x = random.randint(0, (img.size[0]-text_size[0]))  # full width - horizontal size of text
     return x
 
 
 # generating a y coordinate
-def get_y(i):
-    text_size = draw.textsize(i, font=font)
-    y = random.randint(0, (img.size[1]-text_size[1]))  # full width - vertical size of text
+def get_y(text_size):
+    if text_size[1] >= img.size[1]:
+        y = 0
+    else:
+        y = random.randint(0, (img.size[1] - text_size[1]))  # full width - horizontal size of text
     return y
 
 
@@ -60,46 +85,45 @@ def doge(img):
     response = requests.get('http://i.imgur.com/P5t9JNF.png')
     doge = Image.open(BytesIO(response.content))
     # doge = Image.open("doge.png")
-    doge.convert('RGBA')
+    doge.convert('RGB')
     img.paste(doge, (img.size[0]//2-doge.size[0]//2, img.size[1]//2-doge.size[1]//2))
 
 
-def print_text(datas):
-    doge(img)
-    x, y = 0, 0
+def print_text(datas, filename):
+    # doge(img)
+    # x, y = 0, 0
 
     # a list for already used coordinates
     not_empty = []
 
     for i in datas:
+        font = ImageFont.truetype(font_path, default_size+i.size)
         text_size = draw.textsize(i.text, font=font)
-        x = get_x(i.text)
-        y = get_y(i.text)
-        # fill = i.fill
         try:
-            if len(i.fill)==4:  # change when the colors are converted properly!!
-                fill = i.fill
-            else:
-                fill = hex_to_rgb(i.fill)
+            fill = i.fill
         except:
-            fill = "white"
-        # fill = hex_to_rgb(i.fill)
+            fill = "black"
+        x = get_x(text_size)
+        y = get_y(text_size)
+        # if (i.x< RectB.X2 & & RectA.X2 > RectB.X1 & &
+        #     RectA.Y1 < RectB.Y2 & & RectA.Y2 > RectB.Y1)
         for dictionary in not_empty:
             keys = list(dictionary.keys())
             values = list(dictionary.values())
-            while x in keys and y in values:  # checking whether the current x,y is already in use
-                x = get_x(i.text)             # if yes, generating a new x, y
-                y = get_y(i.text)
-
-        draw.text((x, y), i.text, fill=fill, font=ImageFont.truetype(font_path, default_size+5*(i.size)))  # the actual drawing of the text
+            while any(range(x, x+text_size[0])) in keys and any(range(y, y+text_size[1])) in values:  # checking whether the current x,y is already in use
+                x = get_x(text_size)       # if yes, generating a new x, y
+                y = get_y(text_size)
+        draw.text((x, y), i.text, fill=fill, font=font)  # the actual drawing of the text
 
         xr = range(x, x+text_size[0])  # all x coordinates of a single piece of text
         yr = range(y, y+text_size[1])  # all y coordinates ~
         not_empty.append({xr: yr})  # saving all used coordinates to a list
-    img.save('out.png')
+    name = 'out_{}.png'.format(filename)
+    img.save(name)
     return img                      # keys are the range for x, values for y
 
-# print_text(companiess)
-print_text(projects)
+print_text(companies, "companies")
+# print_text(projects, "projects")
+
 
 img.show()

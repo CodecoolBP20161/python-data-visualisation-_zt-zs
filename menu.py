b3a86514bb6ff7fa79f005fa7aa29c1ddf49ed04
cickib <bellakocsis06@gmail.com>
2016-07-14 17:27:43 +0200

    implemented image generating

--- menu.py
+++ menu.py
@@ -1,8 +1,11 @@
 from connect_db import sql_queries
+import image_generator
 import time
+
+
 queries = sql_queries()
-menu = True
-while menu:
+menu = 0
+while not menu:
     print('''
     1. client names tag-cloud based on the number of projects by a company
     2. project names tag-cloud based on the size of the budgets
@@ -11,10 +14,18 @@ while menu:
     ''')
     menu = int(input('Select a menu option, please: '))
     if menu == 1:
-        print(queries[0])  # tag_cloud_1
+        image = image_generator.Text.output(sql_queries(), 0)
+        image_generator.Text.sizing(image)
+        image_generator.print_text(image, "companies")
     elif menu == 2:
-        print(queries[1])  # tag_cloud_2
+        image = image_generator.Text.output(sql_queries(), 1)
+        image_generator.Text.sizing(image)
+        image_generator.print_text(image, "projects")
     elif menu == 3:
-        print(queries[2])  # tag_cloud_3
+        image = image_generator.Text.output(sql_queries(), 2)
+        image_generator.Text.sizing(image)
+        image_generator.print_text(image, "clients")
     elif menu == 4:
-        print(queries[3])     # tag_cloud_4
+        image = image_generator.Text.output(sql_queries(), 3)
+        image_generator.Text.sizing(image)
+        image_generator.print_text(image, "company_hq")
